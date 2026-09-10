657f716f448f0bb81ee0501020d8cf642804ef9d
Samuel Aggor <aggorcode@yahoo.com>
2026-09-10 07:20:47 -0400

    Beginning of the unit conversion calculator.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import UnitConversionPage from './pages/UnitConversionPage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <UnitConversionPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/UnitConversionPage.tsx
@@ -0,0 +1,12 @@
+
+
+function UnitConversionPage() {
+  return (
+    <div>
+      <h1>Unit Conversion Calculator</h1>
+      <p>Welcome to the Unit Conversion Calculator!</p>
+    </div>
+  )
+}
+
+export default UnitConversionPage
